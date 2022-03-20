afb6f213b520656654e32b4aaa0a083cb7d48b26
tarequl007 <emailaddress@yahoo.com>
2022-03-20 10:43:52 -0700

    App.js:

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const myBtn = document.querySelector('.myBtn')
 
 const rulesBox = document.querySelector('.rulesBox')
 
+const exitBtn = document.querySelector('.exitBtn')
+
+
 myBtn.onclick = () => {
 
     rulesBox.classList.add('activeInfo')
 
+}
+
+exitBtn.onclick = () => {
+    rulesBox.classList.remove('activeInfo')
 }
