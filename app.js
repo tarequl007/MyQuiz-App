2e3bad1c7e2a472db1ff105a284f9a581ceef9de
tarequl007 <emailaddress@yahoo.com>
2022-03-20 12:15:06 -0700

    update Question and Option

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const rulesBox = document.querySelector('.rulesBox')
 
 const exitBtn = document.querySelector('.exitBtn')
 
+const continueBtn = document.querySelector('.continueBtn')
+
+const question = document.querySelector('.question')
+
 
 myBtn.onclick = () => {
 
@@ -13,4 +17,30 @@ myBtn.onclick = () => {
 
 exitBtn.onclick = () => {
     rulesBox.classList.remove('activeInfo')
+}
+
+continueBtn.onclick = () => {
+    question.classList.remove('activeInfo')
+    question.classList.add('activeQuiz')
+
+    // Call the Question function
+
+    showQuestion(0)
+}
+
+
+// Question dynamic area
+
+const showQuestion = (index) => {
+    const qText = document.querySelector('.qText')
+    qText.innerHTML = `${questionArray[index].numb} . ${questionArray[index].question}`
+
+    // Option dynamic area
+
+    const myOption = document.querySelector('.myOption');
+    const option = `<div class="option">${questionArray[index].option[0]}<span></span></div> <div class="option">${questionArray[index].option[1]}<span></span></div> <div class="option">${questionArray[index].option[2]}<span></span></div> <div class="option">${questionArray[index].option[3]}<span></span></div>`
+
+    myOption.innerHTML = option;
+
+
 }
---
+++ question.js
@@ -0,0 +1,47 @@
+let questionArray = [{
+        numb: 1,
+        question: 'What does HTML Stand for ?',
+        answer: 'Hyper Text Markup Language',
+        option: [
+            'Hyper Text Multiple Language',
+            'Hyper Text Nasa Language',
+            'Hyper Text Markup Language',
+            'Hyper Text Hack Language'
+        ]
+    },
+    {
+        numb: 2,
+        question: 'What does CSS Stand for ?',
+        answer: 'Cascading Style Sheet',
+        option: [
+            'Common Style Sheet',
+            'Catch Style Sheet',
+            'Coding Style Sheet',
+            'Cascading Style Sheet'
+        ]
+    },
+    {
+        numb: 3,
+        question: 'What does PHP Stand for ?',
+        answer: 'Hypertext Preprocessor',
+        option: [
+            'Hypertext Preprocessor',
+            'Hypertext Processor',
+            'Hypertext Process',
+            'Hypertext Prepration',
+        ]
+    },
+    {
+        numb: 4,
+        question: 'What does SQL Stand for ?',
+        answer: 'Structured Query Language',
+        option: [
+            'Structured Query Language',
+            'Structured Query Large',
+            'Structured Question Language',
+            'Structured Quakka Language'
+        ]
+    }
+
+
+]
