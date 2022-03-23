0710a2aa8feace7347cf48a2ed83df1735c96382
tarequl007 <emailaddress@yahoo.com>
2022-03-22 22:58:54 -0700

    update Option

--- app.js
+++ app.js
@@ -29,32 +29,6 @@ continueBtn.onclick = () => {
 }
 
 
-
-// Question dynamic area
-
-const showQuestion = (index) => {
-    const qText = document.querySelector('.qText')
-    qText.innerHTML = `${questionArray[index].numb} . ${questionArray[index].question}`
-
-    // Option dynamic area
-
-    const myOption = document.querySelector('.myOption');
-    const option = `<div class="option">${questionArray[index].option[0]}<span></span></div> <div class="option">${questionArray[index].option[1]}<span></span></div> <div class="option">${questionArray[index].option[2]}<span></span></div> <div class="option">${questionArray[index].option[3]}<span></span></div>`
-
-    myOption.innerHTML = option;
-
-    // Add footer question list in the page
-
-    const totalQuestion = document.querySelector('.total-Question');
-
-    let totalQuestionTag = `<p>${questionArray[index].numb} of ${questionArray.length} Question</p>`
-
-    totalQuestion.innerHTML = totalQuestionTag;
-
-
-}
-
-
 // Next Quesion button
 
 const nextBtn = document.querySelector('.nextBtn')
@@ -70,3 +44,74 @@ nextBtn.onclick = () => {
 
     }
 }
+
+
+
+// Question dynamic area
+
+const showQuestion = (index) => {
+    const qText = document.querySelector('.qText')
+    qText.innerHTML = `${questionArray[index].numb} . ${questionArray[index].question}`
+
+    // Option dynamic area
+
+    const myOption = document.querySelector('.myOption');
+    let optionTag = `<div class="option">${questionArray[index].option[0]}<span></span></div> <div class="option">${questionArray[index].option[1]}<span></span></div> <div class="option">${questionArray[index].option[2]}<span></span></div> <div class="option">${questionArray[index].option[3]}<span></span></div>`
+
+    myOption.innerHTML = optionTag;
+
+    // Add footer question list in the page
+
+    const totalQuestion = document.querySelector('.total-Question');
+
+    let totalQuestionTag = `<p>${questionArray[index].numb} of ${questionArray.length} Question</p>`
+
+    totalQuestion.innerHTML = totalQuestionTag;
+
+
+
+
+    // Answer update
+    
+    
+    const optionSelect = myOption.querySelectorAll('.option')
+    for (let i = 0; i < optionSelect.length; i++) {
+        optionSelect[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+
+
+}
+
+
+function optionSelected(answer) {
+    let userAns = answer.textContent
+    let correctAnswer = questionArray[questionCount].answer
+    const myOption = document.querySelector('.myOption')
+    let allOption = myOption.children.length;
+
+
+
+    if (userAns == correctAnswer) {
+        answer.classList.add('correct')
+
+
+    } else {
+        answer.classList.add('inCorrect');
+
+        for (let i = 0; i < allOption; i++) {
+            if (myOption.children[i].textContent == correctAnswer) {
+                myOption.children[i].setAttribute('class', 'option correct')
+            }
+        }
+
+    }
+
+
+    for (let i = 0; i < allOption; i++) {
+        myOption.children[i].classList.add('disable')
+    }
+
+}
+
+
+
